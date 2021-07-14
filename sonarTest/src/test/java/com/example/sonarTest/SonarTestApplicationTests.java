e557948fa6b93cf3322535c602671381040145e7
a.tyshkovets <AnTys123#>
2021-07-14 13:31:28 +0300

    init

---
+++ sonarTest/src/test/java/com/example/sonarTest/SonarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonarTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
