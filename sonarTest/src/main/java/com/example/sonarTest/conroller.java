5e8dbdd273e6f0303bce756890fd09282abb7c9c
a.tyshkovets <AnTys123#>
2021-07-14 14:03:05 +0300

    init

---
+++ sonarTest/src/main/java/com/example/sonarTest/conroller.java
@@ -0,0 +1,14 @@
+package com.example.sonarTest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/{systemName}/roles")
+public class conroller {
+    @GetMapping()
+    public void getRoles() {
+        System.out.println("hello");
+    }
+}
